86c8b2f8c5aedf08d04ae56721fbbcc4f82f32c2
Felix <felixlopzd@gmail.com>
2020-07-31 19:46:32 -0400

    added triggers on generar bd

--- generar_bd.sql
+++ generar_bd.sql
@@ -215,3 +215,21 @@ FOREIGN KEY ( `nrc_asignatura` ) REFERENCES `asignatura` ( `nrc` ) ON DELETE CAS
 );
 
 SET FOREIGN_KEY_CHECKS=1;
+
+
+DROP TRIGGER IF EXISTS set_user_role;
+
+CREATE TRIGGER set_user_role 
+	BEFORE INSERT ON usuario 
+	FOR EACH ROW
+BEGIN
+
+	IF (NEW.correo LIKE '%est%') THEN
+		SET NEW.role = "EST";
+	ELSEIF (NEW.correo LIKE "%admin%") THEN
+		SET NEW.role = "ADM";
+	ELSE 
+		SET NEW.role = "PRO";
+  END IF;
+	 
+END
