87c01390c52895418d5cf2f7f81224aed9b5ba14
Andrea Paz <46249374+AndreaToPaz@users.noreply.github.com>
2020-08-10 02:11:44 -0400

    Update asistencia.sql

--- querys_a/asistencia.sql
+++ querys_a/asistencia.sql
@@ -1,16 +1,10 @@
-cedula_usuario INT NOT NULL,
-nrc_asignatura INT NOT NULL,
-id_periodo INT NOT NULL,
-numero_semana INT NOT NULL,
-hora time NOT NULL,
-fecha date NOT NULL,
-asistente INT NOT NULL,
+
 
 -- crear
-INSERT INTO asistencia VALUES (cedula_usuario,nrc_asignatura,id_periodo,numero_semana,hora,fecha,asistente)
+INSERT INTO asistencia VALUES (cedula_usuario,nrc_asignatura,id_periodo,numero_semana,hora,fecha)
 
 -- actualizar toda un lista de una misma asigna
-UPDATE asistencia SET sistente =
+UPDATE asistencia SET asistente =
   WHERE (asistencia.nrc_asignatura = nrc_dado) AND
          (asistencia.id_periodo = id_periodo)
          
